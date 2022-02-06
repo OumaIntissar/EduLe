fe0ba17ada1e2872e8d3139674bb51bcafbebfc5
OumaIntissar <oumaintissar@gmail.com>
2022-02-06 19:57:13 +0100

    Extend SpringBootServletInitializer

--- src/main/java/org/sid/edule/EduLeApplication.java
+++ src/main/java/org/sid/edule/EduLeApplication.java
@@ -2,9 +2,10 @@ package org.sid.edule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EduLeApplication {
+public class EduLeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EduLeApplication.class, args);
